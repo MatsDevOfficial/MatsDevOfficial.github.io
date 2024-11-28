3c07253b97e5c8884243fbea2caa1fbb529378f9
Movermans1 <619401@olz.nl>
2024-11-28 09:51:04 +0100

    KErrst

---
+++ Script.js
@@ -0,0 +1,12 @@
+function createSnowflakes() {
+    const snowflakeCount = 50;
+    for (let i = 0; i < snowflakeCount; i++) {
+        const snowflake = document.createElement('div');
+        snowflake.classList.add('snowflake');
+        snowflake.style.left = `${Math.random() * 100}vw`;
+        snowflake.style.animationDuration = `${Math.random() * 3 + 8}s`;
+        document.body.appendChild(snowflake);
+    }
+}
+
+createSnowflakes();
