b0f894a890dd9eca6fbd88e880e7b3c763b025c5
Movermans1 <619401@olz.nl>
2024-11-28 09:55:44 +0100

    .

--- Script.js
+++ Script.js
@@ -1,12 +1,1 @@
-function createSnowflakes() {
-    const snowflakeCount = 50;
-    for (let i = 0; i < snowflakeCount; i++) {
-        const snowflake = document.createElement('div');
-        snowflake.classList.add('snowflake');
-        snowflake.style.left = `${Math.random() * 100}vw`;
-        snowflake.style.animationDuration = `${Math.random() * 3 + 8}s`;
-        document.body.appendChild(snowflake);
-    }
-}
-
-createSnowflakes();
+// Geen sneeuwvlokken script meer
